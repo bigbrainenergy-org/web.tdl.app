33dc24f5f72db03e6468a005662981c728c7b251
Josh Buker <git-commit@joshbuker.com>
2024-03-12 11:45:10 -0700

    Extract time zone switcher into component

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -13,17 +13,7 @@
       </q-card-section>
 
       <q-card-section class="text-center q-pa-md">
-        <p>Current Time: {{ currentTime }}</p>
-        <q-select
-        v-model="editTimeZone"
-        class="q-my-md"
-        filled
-        @update:model-value="updateTimeZone"
-        :options="timeZones"
-        option-value="value"
-        option-label="name"
-        label="Time Zone"
-        />
+        <TimeZoneSwitcher />
         <LanguageSwitcher />
         <q-btn
           class="q-ma-md"
@@ -59,25 +49,12 @@
 </template>
 
 <script setup lang="ts">
-import {
-  computed,
-  ref,
-  onMounted,
-  onBeforeUnmount,
-  defineComponent
-} from 'vue'
-
+import { defineComponent } from 'vue'
 import { useQuasar } from 'quasar'
-
-import { DateTime } from 'luxon'
-
-import { useRepo } from 'pinia-orm'
-import { useAuthenticationStore } from 'src/stores/authentication/pinia-authentication'
-import { TimeZone, TimeZoneRepo } from 'src/stores/time-zones/time-zone'
-import { UserRepo } from 'src/stores/users/user'
-import FocusModeSettingsDialog from 'src/components/dialog/FocusModeSettingsDialog.vue'
 import { useRouter } from 'vue-router'
-import { Utils } from 'src/util'
+import { useAuthenticationStore } from 'src/stores/authentication/pinia-authentication'
+import FocusModeSettingsDialog from 'src/components/dialog/FocusModeSettingsDialog.vue'
+import TimeZoneSwitcher from 'src/components/TimeZoneSwitcher.vue'
 import LanguageSwitcher from 'src/components/LanguageSwitcher.vue'
 import PasswordChangeForm from 'src/components/PasswordChangeForm.vue'
 
@@ -91,77 +68,6 @@ if(authenticationStore.isLoggedIn !== true) {
 }
 
 const $q = useQuasar()
-const tzr = useRepo(TimeZoneRepo)
-const userRepo = computed(() => useRepo(UserRepo))
-
-const currentTime = ref(DateTime.local().toFormat('h:mm:ss a ZZZZ'))
-
-
-const userTimeZone = computed(
-  () => Utils.hardCheck(userRepo.value.getUser(), 'no user found in authentication store or user repo').timeZoneObj
-)
-
-const timeZones = computed(
-  () => tzr.all()
-)
-
-const user = computed(() => Utils.hardCheck(userRepo.value.getUser(), 'user not found in authentication store or user repo'))
-
-function timeZoneName(tzToFind: any) {
-  if (timeZones.value.length > 0) {
-    return timeZones.value.find(
-      (tz) => {
-        return tz.value === tzToFind
-      }
-    )?.name
-  } else {
-    return undefined
-  }
-}
-
-const editTimeZone = ref<TimeZone>(Utils.hardCheck(Utils.hardCheck(userRepo.value.getUser(), 'user not found').timeZoneObj))
-
-const updateTimeZone = async () => {
-  await userRepo.value.changeTimezone(editTimeZone.value as TimeZone)
-}
-
-let timer: NodeJS.Timeout | null = null
-
-function updateCurrentTime() {
-  currentTime.value = DateTime.local().toFormat('h:mm:ss a ZZZZ')
-}
-
-// If timer is active, deactivate it to free up memory / CPU.
-function clearTimer() {
-  if(timer) {
-    clearInterval(timer)
-    timer = null
-  }
-}
-
-// If using KeepAlive to cache in the MainLayout, switch to onActivated
-//onActivated(
-onMounted(
-  () =>{
-    // Immediately update so the user doesn't notice a huge time jump after 1 second
-    updateCurrentTime()
-    // Restart timer
-    timer = setInterval(
-      () => {
-        updateCurrentTime()
-      },
-      1000
-    )
-  }
-)
-
-// Uncomment if using KeepAlive in MainLayout to cache
-// onDeactivated(
-//   () => { clearTimer() }
-// )
-onBeforeUnmount(
-  () => { clearTimer() }
-)
 
 const openFocusModeSettingsDialog = () => {
   $q.dialog({
