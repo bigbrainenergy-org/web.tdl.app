86c1d2292222a4a0451d94060a8473d1ad7eaaba
Josh Buker <crypto@joshbuker.com>
2024-07-19 12:42:17 -0700

    Add e2e placeholder spec for opening task dialog on tasks page

--- test/cypress/e2e/tasks-page.cy.js
+++ test/cypress/e2e/tasks-page.cy.js
@@ -1,4 +1,5 @@
 describe('Tasks page', () => {
+  it('should open tasks dialog when a task is clicked')
   it('should allow filtering current tasks by their attributes')
   it('should allow sorting current tasks by their attributes')
 })
