3ca3b4944bd7111738253112555c0787c60a91ce
Josh Buker <crypto@joshbuker.com>
2024-09-23 16:55:51 -0700

    Remove PWA mode due to outdated deps

--- quasar.config.js
+++ quasar.config.js
@@ -147,41 +147,6 @@ export default configure(function (/* ctx */) {
     //   electronPreload: 'src-electron/electron-preload'
     // },
 
-    // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
-    // ssr: {
-    //   // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-    //                                       // will mess up SSR
-
-    //   // extendSSRWebserverConf (esbuildConf) {},
-    //   // extendPackageJson (json) {},
-
-    //   pwa: false,
-
-    //   // manualStoreHydration: true,
-    //   // manualPostHydrationTrigger: true,
-
-    //   prodPort: 3000, // The default port that the production server should use
-    //                   // (gets superseded if process.env.PORT is specified at runtime)
-
-    //   middlewares: [
-    //     'render' // keep this as last one
-    //   ]
-    // },
-
-    // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
-    pwa: {
-      workboxMode: 'generateSW', // or 'injectManifest'
-      injectPwaMetaTags: true,
-      swFilename: 'sw.js',
-      manifestFilename: 'manifest.json',
-      useCredentialsForManifestTag: false
-      // useFilenameHashes: true,
-      // extendGenerateSWOptions (cfg) {}
-      // extendInjectManifestOptions (cfg) {},
-      // extendManifestJson (json) {}
-      // extendPWACustomSWConf (esbuildConf) {}
-    },
-
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-cordova-apps/configuring-cordova
     cordova: {
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
